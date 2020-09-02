896d43815c3d66b7bdbd6eb73463040a092fc09d
hoomzfwp <ndamte@gmail.com>
2020-09-01 20:10:40 -0500

    Compile the code in a scratch directory.
    I was not able to figure out how to do it in the prefered path.

---
+++ compiling/extract_and_compile.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Hoomz Damte
+# University of Minnesota, Morris
+# August 2020
+
+input=$1
+
+scratch=$(mktemp --directory)
+
+tmpDir=$(mkdir NthPrime)
+
+tar zxf NthPrime.tgz -C $scratch
+
+gcc $scratch/NthPrime/main.c $scratch/NthPrime/nth_prime.c -o $scratch/NthPrime/NthPrime
+
+output=$($scratch/NthPrime/NthPrime $input)
+
+echo $output
